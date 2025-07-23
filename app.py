158ccd405439bc2af88dbc32ca3040717ed642ba
Soumya <sowmyah871@gmail.com>
2025-07-23 19:03:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ## Arxiv and wikipedia tools
-arxiv_wrapper=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=500)
+arxiv_wrapper=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=250)
 arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
 search=DuckDuckGoSearchRun(name="search")
 
